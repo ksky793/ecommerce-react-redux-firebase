33adf3c710fd924138f586f0e92a50494fc868be
ksky793 <k.sky793@gmail.com>
2023-01-31 02:58:13 +0100

    Dodano domyślny kod do komponentów w pages

---
+++ src/pages/auth/LoginPage.jsx
@@ -0,0 +1,11 @@
+import LoginForm from '../../components/forms/Login/LoginForm';
+
+const LoginPage = (props) => {
+	return (
+		<div>
+			<LoginForm {...props} />
+		</div>
+	);
+};
+
+export default LoginPage;
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,8 +1,9 @@
 import Header from '../../components/Header/Header';
 import AboutProducts from '../../components/AboutProducts/AboutProducts';
-const HomePage = () => {
+const HomePage = (props) => {
 	return (
 		<div className='home-page'>
+			<h1>Witaj {props.user}!</h1>
 			<Header />
 			<AboutProducts />
 		</div>
