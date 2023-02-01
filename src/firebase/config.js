1fbde99923ae236b8302d7dbea292b1a4519ad2c
ksky793 <k.sky793@gmail.com>
2023-02-01 08:36:30 +0100

    Konfiguracja firebase / dodanie logowania

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+	apiKey: 'AIzaSyANHllvfh_sr4Owmuf2WUtU0CkmcpJyA6o',
+	authDomain: 'ecommerce-cleaning-products.firebaseapp.com',
+	projectId: 'ecommerce-cleaning-products',
+	storageBucket: 'ecommerce-cleaning-products.appspot.com',
+	messagingSenderId: '615171008842',
+	appId: '1:615171008842:web:5a6b37a2819169580079b3',
+};
+
+
---
+++ src/firebase/utils.js
@@ -0,0 +1,76 @@
+import { firebaseConfig } from './config';
+
+import { initializeApp } from '@firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import {
+	GoogleAuthProvider,
+	signInWithEmailAndPassword,
+	signInWithPopup,
+	createUserWithEmailAndPassword,
+	sendPasswordResetEmail,
+	signOut,
+} from 'firebase/auth';
+import { query, getDocs, collection, where, addDoc } from 'firebase/firestore';
+
+// połączenie z baza firebase
+const app = initializeApp(firebaseConfig);
+
+// inicjalizacja
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
+
+// logowanie z google
+const googleProvider = new GoogleAuthProvider();
+export const signInWithGoogle = async () => {
+	try {
+		const res = await signInWithPopup(auth, googleProvider);
+		const user = res.user;
+
+		const q = query(collection(db, 'users'), where('uid', '==', user.uid));
+		const docs = await getDocs(q);
+		if (docs.docs.length === 0) {
+			await addDoc(collection(db, 'users'), {
+				uid: user.uid,
+				name: user.displayName,
+				authProvider: 'google',
+				email: user.email,
+			});
+		}
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+// logowanie za pomoca maila i hasła
+export function logInWithEmailAndPassword(email, password) {
+	return new Promise((resolve, reject) => {
+		const data = signInWithEmailAndPassword(auth, email, password);
+		if (data) {
+			resolve(data);
+		} else {
+			reject();
+		}
+	});
+}
+
+// resetowanie hasła
+export const sendPasswordReset = async (email) => {
+	try {
+		await sendPasswordResetEmail(auth, email);
+		alert('Password reset link sent!');
+	} catch (err) {
+		console.error(err);
+		alert(err.message);
+	}
+};
+
+export const logout = async () => {
+	await signOut(auth);
+};
+
+// rejestracja za pomocą emaila i hasła
