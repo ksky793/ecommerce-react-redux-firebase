1ae1da764a61c59ec33730aff9d9a91df990739b
ksky793 <k.sky793@gmail.com>
2023-03-16 22:29:49 +0100

    Dodano defaultowe pliki pomocnicze scss

--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import ReactDOM from 'react-dom/client';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { BrowserRouter as Router } from 'react-router-dom';
-library.add(faSpinner);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<Router>
-				<App />
-			</Router>
-		</Provider>
-	</React.StrictMode>
+	// <React.StrictMode>
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>
+	// </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
